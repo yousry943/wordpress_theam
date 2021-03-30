861e8d1c49f3e430eb7d7b15413f2b0a4eb63108
ayousry943 <ayousry943@gmail.com>
2021-03-30 16:11:22 +0200

    s

--- wp-content/themes/FlipaMart/index.php
+++ wp-content/themes/FlipaMart/index.php
@@ -31,50 +31,45 @@
 					<div id="hero">
 						<div id="owl-main" class="owl-carousel owl-inner-nav owl-ui-sm">
 
-							<div class="item" style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/sliders/01.jpg);">
-								<div class="container-fluid">
-									<div class="caption bg-color vertical-center text-left">
-										<div class="slider-header fadeInDown-1">Top Brands</div>
-										<div class="big-text fadeInDown-1">
-											New Collections
-										</div>
 
-										<div class="excerpt fadeInDown-2 hidden-xs">
+							<?php $slider = get_posts(array('post_type' => 'slider', 'posts_per_page' => 5)); ?>
+							<?php $count = 0; ?>
+							<?php foreach ($slider as $slide) : ?>
 
-											<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+								<div class="item" style="background-image: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id($slide->ID)) ?>);">
+									<div class="container-fluid">
+										<div class="caption bg-color vertical-center text-left">
+											<div class="slider-header fadeInDown-1">Top Brands</div>
+											<div class="big-text fadeInDown-1">
+												New Collections
+											</div>
 
-										</div>
-										<div class="button-holder fadeInDown-3">
-											<a href="index.php?page=single-product" class="btn-lg btn btn-uppercase btn-primary shop-now-button">Shop Now</a>
-										</div>
-									</div><!-- /.caption -->
-								</div><!-- /.container-fluid -->
-							</div><!-- /.item -->
+											<div class="excerpt fadeInDown-2 hidden-xs">
 
-							<div class="item" style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/sliders/02.jpg);">
-								<div class="container-fluid">
-									<div class="caption bg-color vertical-center text-left">
-										<div class="slider-header fadeInDown-1">Spring 2016</div>
-										<div class="big-text fadeInDown-1">
-											Women <span class="highlight">Fashion</span>
-										</div>
+												<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
 
-										<div class="excerpt fadeInDown-2 hidden-xs">
+											</div>
+											<div class="button-holder fadeInDown-3">
+												<a href="index.php?page=single-product" class="btn-lg btn btn-uppercase btn-primary shop-now-button">Shop Now</a>
+											</div>
+										</div><!-- /.caption -->
+									</div><!-- /.container-fluid -->
+								</div><!-- /.item -->
 
-											<span>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit</span>
 
-										</div>
-										<div class="button-holder fadeInDown-3">
-											<a href="index.php?page=single-product" class="btn-lg btn btn-uppercase btn-primary shop-now-button">Shop Now</a>
-										</div>
-									</div><!-- /.caption -->
-								</div><!-- /.container-fluid -->
-							</div><!-- /.item -->
+								<?php $count++; ?>
+							<?php endforeach; ?>
+
+
+
 
 
 
 						</div><!-- /.owl-carousel -->
 					</div>
+
+
+
 
 					<!-- ========================================= SECTION – HERO : END ========================================= -->
 
@@ -129,8 +124,7 @@
 							<ul class="nav nav-tabs nav-tab-line pull-right" id="new-products-1">
 								<li class="active"><a data-transition-type="backSlide" href="#all" data-toggle="tab">All</a></li>
 								<li><a data-transition-type="backSlide" href="#smartphone" data-toggle="tab">Clothing</a></li>
-								<li><a data-transition-type="backSlide" href="#laptop" data-toggle="tab">Electronics</a></li>
-								<li><a data-transition-type="backSlide" href="#apple" data-toggle="tab">Shoes</a></li>
+
 							</ul><!-- /.nav-tabs -->
 						</div>
 
@@ -1642,8 +1636,8 @@
 												<div class="blog-post-image">
 													<div class="image">
 														<a href="<?php the_permalink(); ?>">
-													<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'thumbnail' ); ?>
-                                                  <img src="<?php echo $url ?>" />
+															<?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID), 'thumbnail'); ?>
+															<img src="<?php echo $url ?>" />
 														</a>
 													</div>
 												</div><!-- /.blog-post-image -->
--- wp-content/themes/FlipaMart/left-sidebar.php
+++ wp-content/themes/FlipaMart/left-sidebar.php
@@ -46,27 +46,32 @@
 				<h3 class="section-title">Special Offer </h3>
 				<div class="sidebar-widget-body outer-top-xs">
 					<div class="owl-carousel sidebar-carousel special-offer custom-carousel owl-theme outer-top-xs">
+
+
 						<div class="item">
-							<div class="products special-product">
+
+							<?php
+							$args = array(
+								'post_type' => 'product',
+								'meta_key'       => '_sale_price',
+								'orderby' => 'meta_value_num',
+								'posts_per_page' => 5,
+								'order' => 'DESC',
 
 
-								<?php
-								$args = array(
-									'post_type' => 'product',
-									'meta_key'       => '_sale_price',
-									'orderby' => 'meta_value_num',
-
-								);
-								$query = new WP_Query($args);
-								global $product;
+							);
+							$query = new WP_Query($args);
+							global $product;
 
 
-								?>
+							?>
 
 
-								<?php if ($query->have_posts()) : ?>
-									<?php while ($query->have_posts()) : $query->the_post(); ?>
-										<?php $product = wc_get_product(get_the_ID()); ?>
+							<?php if ($query->have_posts()) : ?>
+								<?php while ($query->have_posts()) : $query->the_post(); ?>
+									<?php $product = wc_get_product(get_the_ID()); ?>
+									<div class="products special-product">
+
 
 										<div class="product">
 											<div class="product-micro">
@@ -90,7 +95,7 @@
 													<div class="col col-xs-7">
 														<div class="product-info">
 															<h3 class="name"><a href="<?php the_permalink(); ?>"><?php the_title(); ?> </a></h3>
-															<div class="rating rateit-small"></div>
+
 															<div class="product-price">
 																<span class="price">
 																	<?php echo wc_price(wc_get_price_including_tax($product)); ?> </span>
@@ -106,16 +111,18 @@
 
 										</div>
 
-									<?php endwhile; ?>
-
-								<?php endif; ?>
 
 
 
 
+									</div>
+								<?php endwhile; ?>
 
-							</div>
+							<?php endif; ?>
+
 						</div>
+
+
 
 					</div>
 				</div><!-- /.sidebar-widget-body -->
@@ -171,14 +178,13 @@
 						$tag_slug = str_replace(" ", "-", $unique);
 						$tag_slug = strtolower(str_replace($special_characters, "", $tag_slug));
 						echo '<a  class="item " > ' . $unique . '</a>';
-					
 					}
 					echo '</div>';
 
 					//Reset the query
 					wp_reset_postdata();
 					?>
-				
+
 				</div><!-- /.sidebar-widget-body -->
 			</div><!-- /.sidebar-widget -->
 			<!-- ============================================== PRODUCT TAGS : END ============================================== -->
@@ -188,292 +194,73 @@
 				<h3 class="section-title">Special Deals</h3>
 				<div class="sidebar-widget-body outer-top-xs">
 					<div class="owl-carousel sidebar-carousel special-offer custom-carousel owl-theme outer-top-xs">
+
+
 						<div class="item">
-							<div class="products special-product">
-								<div class="product">
-									<div class="product-micro">
-										<div class="row product-micro-row">
-											<div class="col col-xs-5">
-												<div class="product-image">
-													<div class="image">
-														<a href="#">
-															<img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/p28.jpg" alt="">
-														</a>
-													</div><!-- /.image -->
+							<?php
+							$args = array(
+								'post_type' => 'product',
+								'meta_key'       => '_sale_price',
+								'orderby' => 'meta_value_num',
+								'posts_per_page' => 5,
+								'order' => 'ASC',
 
 
-												</div><!-- /.product-image -->
-											</div><!-- /.col -->
-											<div class="col col-xs-7">
-												<div class="product-info">
-													<h3 class="name"><a href="#">Floral Print Shirt</a></h3>
-													<div class="rating rateit-small"></div>
-													<div class="product-price">
-														<span class="price">
-															$450.99 </span>
-
-													</div><!-- /.product-price -->
-
-												</div>
-											</div><!-- /.col -->
-										</div><!-- /.product-micro-row -->
-									</div><!-- /.product-micro -->
-
-								</div>
-								<div class="product">
-									<div class="product-micro">
-										<div class="row product-micro-row">
-											<div class="col col-xs-5">
-												<div class="product-image">
-													<div class="image">
-														<a href="#">
-															<img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/p15.jpg" alt="">
-														</a>
-													</div><!-- /.image -->
+							);
+							$query = new WP_Query($args);
+							global $product;
 
 
-												</div><!-- /.product-image -->
-											</div><!-- /.col -->
-											<div class="col col-xs-7">
-												<div class="product-info">
-													<h3 class="name"><a href="#">Floral Print Shirt</a></h3>
-													<div class="rating rateit-small"></div>
-													<div class="product-price">
-														<span class="price">
-															$450.99 </span>
-
-													</div><!-- /.product-price -->
-
-												</div>
-											</div><!-- /.col -->
-										</div><!-- /.product-micro-row -->
-									</div><!-- /.product-micro -->
-
-								</div>
-								<div class="product">
-									<div class="product-micro">
-										<div class="row product-micro-row">
-											<div class="col col-xs-5">
-												<div class="product-image">
-													<div class="image">
-														<a href="#">
-															<img data-echo="<?php echo get_template_directory_uri(); ?>/assets/images/products/p26.jpg" alt="">
-														</a>
-													</div><!-- /.image -->
+							?>
 
 
-												</div><!-- /.product-image -->
-											</div><!-- /.col -->
-											<div class="col col-xs-7">
-												<div class="product-info">
-													<h3 class="name"><a href="#">Floral Print Shirt</a></h3>
-													<div class="rating rateit-small"></div>
-													<div class="product-price">
-														<span class="price">
-															$450.99 </span>
+							<?php if ($query->have_posts()) : ?>
+								<?php while ($query->have_posts()) : $query->the_post(); ?>
+									<?php $product = wc_get_product(get_the_ID()); ?>
+									<div class="products special-product">
 
-													</div><!-- /.product-price -->
+										<div class="product">
+											<div class="product-micro">
+												<div class="row product-micro-row">
+													<div class="col col-xs-5">
+														<div class="product-image">
+															<div class="image">
+																<a href="<?php the_permalink(); ?>">
+																	<?php the_post_thumbnail(); ?>
+																	<div class="zoom-overlay"></div>
+																</a>
+															</div><!-- /.image -->
 
-												</div>
-											</div><!-- /.col -->
-										</div><!-- /.product-micro-row -->
-									</div><!-- /.product-micro -->
 
-								</div>
-							</div>
+
+														</div><!-- /.product-image -->
+													</div><!-- /.col -->
+													<div class="col col-xs-7">
+														<div class="product-info">
+															<h3 class="name"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+															<div class="rating rateit-small"></div>
+															<div class="product-price">
+																<span class="price">
+																	<?php echo wc_price(wc_get_price_including_tax($product)); ?> </span>
+
+															</div><!-- /.product-price -->
+
+														</div>
+													</div><!-- /.col -->
+												</div><!-- /.product-micro-row -->
+											</div><!-- /.product-micro -->
+
+										</div>
+
+									</div>
+								<?php endwhile; ?>
+
+							<?php endif; ?>
+
 						</div>
-						<div class="item">
-							<div class="products special-product">
-								<div class="product">
-									<div class="product-micro">
-										<div class="row product-micro-row">
-											<div class="col col-xs-5">
-												<div class="product-image">
-													<div class="image">
-														<a href="#">
-															<img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/p18.jpg" alt="">
-														</a>
-													</div><!-- /.image -->
-
-
-												</div><!-- /.product-image -->
-											</div><!-- /.col -->
-											<div class="col col-xs-7">
-												<div class="product-info">
-													<h3 class="name"><a href="#">Floral Print Shirt</a></h3>
-													<div class="rating rateit-small"></div>
-													<div class="product-price">
-														<span class="price">
-															$450.99 </span>
-
-													</div><!-- /.product-price -->
-
-												</div>
-											</div><!-- /.col -->
-										</div><!-- /.product-micro-row -->
-									</div><!-- /.product-micro -->
-
-								</div>
-								<div class="product">
-									<div class="product-micro">
-										<div class="row product-micro-row">
-											<div class="col col-xs-5">
-												<div class="product-image">
-													<div class="image">
-														<a href="#">
-															<img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/p17.jpg" alt="">
-														</a>
-													</div><!-- /.image -->
 
 
 
-												</div><!-- /.product-image -->
-											</div><!-- /.col -->
-											<div class="col col-xs-7">
-												<div class="product-info">
-													<h3 class="name"><a href="#">Floral Print Shirt</a></h3>
-													<div class="rating rateit-small"></div>
-													<div class="product-price">
-														<span class="price">
-															$450.99 </span>
-
-													</div><!-- /.product-price -->
-
-												</div>
-											</div><!-- /.col -->
-										</div><!-- /.product-micro-row -->
-									</div><!-- /.product-micro -->
-
-								</div>
-								<div class="product">
-									<div class="product-micro">
-										<div class="row product-micro-row">
-											<div class="col col-xs-5">
-												<div class="product-image">
-													<div class="image">
-														<a href="#">
-															<img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/p16.jpg" alt="">
-														</a>
-													</div><!-- /.image -->
-
-
-												</div><!-- /.product-image -->
-											</div><!-- /.col -->
-											<div class="col col-xs-7">
-												<div class="product-info">
-													<h3 class="name"><a href="#">Floral Print Shirt</a></h3>
-													<div class="rating rateit-small"></div>
-													<div class="product-price">
-														<span class="price">
-															$450.99 </span>
-
-													</div><!-- /.product-price -->
-												</div>
-											</div><!-- /.col -->
-										</div><!-- /.product-micro-row -->
-									</div><!-- /.product-micro -->
-
-								</div>
-							</div>
-						</div>
-						<div class="item">
-							<div class="products special-product">
-								<div class="product">
-									<div class="product-micro">
-										<div class="row product-micro-row">
-											<div class="col col-xs-5">
-												<div class="product-image">
-													<div class="image">
-														<a href="#">
-															<img data-echo="<?php echo get_template_directory_uri(); ?>/assets/images/products/p15.jpg" alt="">
-															<div class="zoom-overlay"></div>
-														</a>
-													</div><!-- /.image -->
-
-
-
-												</div><!-- /.product-image -->
-											</div><!-- /.col -->
-											<div class="col col-xs-7">
-												<div class="product-info">
-													<h3 class="name"><a href="#">Floral Print Shirt</a></h3>
-													<div class="rating rateit-small"></div>
-													<div class="product-price">
-														<span class="price">
-															$450.99 </span>
-
-													</div><!-- /.product-price -->
-
-												</div>
-											</div><!-- /.col -->
-										</div><!-- /.product-micro-row -->
-									</div><!-- /.product-micro -->
-
-								</div>
-								<div class="product">
-									<div class="product-micro">
-										<div class="row product-micro-row">
-											<div class="col col-xs-5">
-												<div class="product-image">
-													<div class="image">
-														<a href="#">
-															<img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/p14.jpg" alt="">
-															<div class="zoom-overlay"></div>
-														</a>
-													</div><!-- /.image -->
-
-
-
-												</div><!-- /.product-image -->
-											</div><!-- /.col -->
-											<div class="col col-xs-7">
-												<div class="product-info">
-													<h3 class="name"><a href="#">Floral Print Shirt</a></h3>
-													<div class="rating rateit-small"></div>
-													<div class="product-price">
-														<span class="price">
-															$450.99 </span>
-
-													</div><!-- /.product-price -->
-
-												</div>
-											</div><!-- /.col -->
-										</div><!-- /.product-micro-row -->
-									</div><!-- /.product-micro -->
-
-								</div>
-								<div class="product">
-									<div class="product-micro">
-										<div class="row product-micro-row">
-											<div class="col col-xs-5">
-												<div class="product-image">
-													<div class="image">
-														<a href="#">
-															<img src="<?php echo get_template_directory_uri(); ?>/assets/images/products/p13.jpg" alt="">
-														</a>
-													</div><!-- /.image -->
-
-
-												</div><!-- /.product-image -->
-											</div><!-- /.col -->
-											<div class="col col-xs-7">
-												<div class="product-info">
-													<h3 class="name"><a href="#">Floral Print Shirt</a></h3>
-													<div class="rating rateit-small"></div>
-													<div class="product-price">
-														<span class="price">
-															$450.99 </span>
-
-													</div><!-- /.product-price -->
-
-												</div>
-											</div><!-- /.col -->
-										</div><!-- /.product-micro-row -->
-									</div><!-- /.product-micro -->
-
-								</div>
-							</div>
-						</div>
 					</div>
 				</div><!-- /.sidebar-widget-body -->
 			</div><!-- /.sidebar-widget -->
