665640b75b3eb2eb1dec31ae07fe526c80b16c93
ayousry943 <ayousry943@gmail.com>
2021-05-20 16:29:36 +0200

    edit

--- wp-content/plugins/Fcalculator/calculator.php
+++ wp-content/plugins/Fcalculator/calculator.php
@@ -1,5 +1,79 @@
 <?php
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              ayousry943@gmail.com
+ * @since             1.0.0
+ * @package           FCalculator
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Fcalculator
+ * Plugin URI:        Fcalculator.com
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            Ahmed yousry
+ * Author URI:        ayousry943@gmail.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       Fcalculator
+ * Domain Path:       /languages
+ */
 
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define('CALCULATOR_VERSION', '1.0.0');
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-calculator-activator.php
+ */
+function activate_calculator()
+{
+	require_once plugin_dir_path(__FILE__) . 'includes/class-calculator-activator.php';
+	Calculator_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-calculator-deactivator.php
+ */
+function deactivate_calculator()
+{
+	require_once plugin_dir_path(__FILE__) . 'includes/class-calculator-deactivator.php';
+	Calculator_Deactivator::deactivate();
+}
+
+register_activation_hook(__FILE__, 'activate_calculator');
+register_deactivation_hook(__FILE__, 'deactivate_calculator');
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path(__FILE__) . 'includes/class-calculator.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
 class Short
 {
 
@@ -215,81 +289,6 @@ class Short
 		return $content;
 	}
 }
-/**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              ayousry943@gmail.com
- * @since             1.0.0
- * @package           FCalculator
- *
- * @wordpress-plugin
- * Plugin Name:       Fcalculator
- * Plugin URI:        Fcalculator.com
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
- * Author:            Ahmed yousry
- * Author URI:        ayousry943@gmail.com
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       Fcalculator
- * Domain Path:       /languages
- */
-
-// If this file is called directly, abort.
-if (!defined('WPINC')) {
-	die;
-}
-
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
-define('CALCULATOR_VERSION', '1.0.0');
-
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-calculator-activator.php
- */
-function activate_calculator()
-{
-	require_once plugin_dir_path(__FILE__) . 'includes/class-calculator-activator.php';
-	Calculator_Activator::activate();
-}
-
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-calculator-deactivator.php
- */
-function deactivate_calculator()
-{
-	require_once plugin_dir_path(__FILE__) . 'includes/class-calculator-deactivator.php';
-	Calculator_Deactivator::deactivate();
-}
-
-register_activation_hook(__FILE__, 'activate_calculator');
-register_deactivation_hook(__FILE__, 'deactivate_calculator');
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path(__FILE__) . 'includes/class-calculator.php';
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
 $Calculator_Admin = new Short();
 
 function run_calculator()
